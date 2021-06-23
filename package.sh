ab4b3b2e32898d57f5aa4aba84ca91200c5ecd68
toddiuzho <todd.trimmer@gmail.com>
2021-06-22 22:48:11 -0500

    bump major and minor. leading-zero validation of semver

--- package.sh
+++ package.sh
@@ -1,13 +1,41 @@
+#=import os
+
+bump-major() {
+  local sv="${1}"
+  [ -z "${sv}" ] && return 1
+  local jv="${sv%%.*}"
+  local npv="${sv#*.}"
+  [ "${jv}" = "${sv}" ] && return 1
+  [ "${npv}" = "${sv}" ] && return 1
+  declare -i ijv=${jv}
+  ijv=$(( $ijv + 1 ))
+  echo "${ijv}.${npv}"
+}
+
 bump-patch() {
   local sv="${1}"
   [ -z "${sv}" ] && return 1
   local pv="${sv##*.}"
-  jnv="${sv%.*}"
+  local jnv="${sv%.*}"
   [ "${pv}" = "${sv}" ] && return 1
   [ "${jnv}" = "${sv}" ] && return 1
   declare -i ipv=${pv}
   ipv=$(( $ipv + 1 ))
   echo "${jnv}.${ipv}"
+}
+
+bump-minor() {
+  local sv="${1}"
+  [ -z "${sv}" ] && return 1
+  local jv="${sv%%.*}"
+  local npv="${sv#*.}"
+  local nv="${npv%.*}"
+  [ "${jv}" = "${sv}" ] && return 1
+  [ "${npv}" = "${sv}" ] && return 1
+  [ "${nv}" = "${npv}" ] && return 1
+  declare -i inv=${nv}
+  inv=$(( $inv + 1 ))
+  echo "${jv}.${inv}.${pv}"
 }
 
 validate-semver() {
@@ -22,6 +50,20 @@ validate-semver() {
     echo "Version [${sv}] is not a semantic version!" >&2
     return 255
   fi
+
+  jnv="${gv%.*}"
+  jv="${jnv%.*}"
+  nv="${jnv#*.}"
+  pv="${gv##*.}"
+
+  for expr in "major=${jv}" "minor=${nv}" "patch=${pv}"; do
+    part="${expr%=*}"
+    xv="${expr#*=}"
+    if [ ${#xv} -gt 1 ] && [ '0' = "${xv:0:1}" ]; then
+      echo "The ${part} version [${xv}] cannot have leading zeroes, unless it is exactly '0' !" >&2
+      return 255
+    fi
+  done
+
   return 0
-  # TODO: no leading zeroes in a part unless it's exactly one zero
 }
